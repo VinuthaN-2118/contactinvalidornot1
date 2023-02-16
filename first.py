89b9acd993dda0d4de86e0d37e7c30d1018989cd
VinuthaN-2118 <120298632+VinuthaN-2118@users.noreply.github.com>
2023-02-16 13:35:33 +0530

    Delete first.py

--- first.py
+++
@@ -1,9 +0,0 @@
-import re
-def isvalid(s):
-    Pattern = re.compile("(0|91)?[6-9] [0-9] {9}")
-    return Pattern.match(s)
-s = "2124567"
-if (isvalid(s)):
-    print("valid number")
-else:
-    print("Invalid number")
